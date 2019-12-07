fe38492474a8087dc5e1fe10d432196a15e6b0d3
busisd <busisd@carleton.edu>
2019-12-06 23:37:46 -0500

    Cleaning up old comments

--- artifact_script.py
+++ artifact_script.py
@@ -3,16 +3,6 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('./testchar1_235364326')
 root = tree.getroot()
 locations = root.find("locations")
-beach = list(locations)[14]
-print(list(locations)[14].find("name").text)
-beach_obj = beach.find("objects")
-list(beach_obj)[1].find("value").find("Object").find("name").text #artifact spot!
-
-list(list(beach_obj)[1].find("key"))[0][0].text # x-coord
-list(list(beach_obj)[1].find("key"))[0][1].text # y-coord
-
-
-
 for loc in list(locations):
 	objs = loc.find("objects")
 	if objs:
@@ -20,11 +10,3 @@ for loc in list(locations):
 			if (item.find("value").find("Object").find("name").text.lower() == "artifact spot"):
 				print(loc.find("name").text+": ("+item.find("key")[0][0].text+", "+item.find("key")[0][1].text+")")
 				
-# for child in tree.getroot():
-	# print(child.tag, child.attrib)
-
-# for child in tree.getroot().iter():
-	# if(child.tag == 'willDestroyObjectsUnderfoot'):
-		# print(child.tag, child.attrib)
-
-# list(locations)[0].find("name").text

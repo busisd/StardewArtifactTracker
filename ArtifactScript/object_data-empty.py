6aed05b1883a22cd2f65f1f0821b21a802412d3b
busisd <busisd@carleton.edu>
2019-12-08 19:25:46 -0500

    Adding empty templates for the data

---
+++ ArtifactScript/object_data-empty.py
@@ -0,0 +1,14 @@
+data = {
+'''
+The data variable above should be set to the value of the unpacked version of ObjectInformation.xnb.
+A quick guide is as follows:
+ - Find the file ObjectInformation.xnb
+   > Try C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Content\Data
+ - Unpack ObjectInformation.xnb into a .json file
+   > https://stardewvalleywiki.com/Modding:Editing_XNB_files#Unpack_game_files gives a good tutorial
+     for how to do this
+ - Copy the dictionary that appears in "content": { ... } and paste it into this file, so that
+   data will be equal to that dictionary
+ - Rename this file to object_data.py by removing the '-empty' in the filename
+'''
+}
